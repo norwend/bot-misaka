5fe89bd0f6f0898ffc5e3eced45c1e650b2ecc4e
Stepan Ilyin <norwend@tuta.io>
2023-03-30 02:40:13 +0300

    added buttons

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+import telegram
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -8,8 +9,21 @@ logging.basicConfig(
 )
 
 
+# Информация, которая должна быть закреплена за клавиатурой:
+# Пост ВК (его айди)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    button_list = [
+        ["Случайный пост", "Отправить в отложку"]
+    ]
+
+    reply_markup = telegram.ReplyKeyboardMarkup(button_list)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Выберите опцию:",
+        reply_markup=reply_markup
+    )
+    # await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 TOKEN = open("token.txt").readline().strip()
 
