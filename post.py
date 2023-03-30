755e1c29b4338074004a5878f1b9f978bf5278ab
Stepan Ilyin <norwend@tuta.io>
2023-03-30 08:25:55 +0300

    removed redundant Bot

--- post.py
+++ post.py
@@ -2,10 +2,8 @@ class Post:
     text: str
     img_url: str
     time: str
-    bot: Bot
 
-    def __init__(self, text: str, img_url: str, time: str, bot: Bot):
+    def __init__(self, text: str, img_url: str, time: str):
         self.text = text
         self.img_url = img_url
         self.time = time
-        self.bot = bot
